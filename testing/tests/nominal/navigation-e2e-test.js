ff5db0c025fcd287dcd50ac20a4b86dd4aa6d698
David B <fishing_junk@yahoo.fr>
2014-01-29 14:07:46 +0100

    wip

---
+++ testing/tests/nominal/navigation-e2e-test.js
@@ -0,0 +1,12 @@
+/* global browser */
+/* global it */
+/* global expect */
+/* global describe */
+/* jslint node: true */
+"use strict";
+
+describe('Navigation', function() {
+	it('Should access to the application', function() {
+
+	});
+});
